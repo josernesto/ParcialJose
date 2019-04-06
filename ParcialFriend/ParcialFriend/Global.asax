632416a8d337d10df4b6dfc80c86160654ae223c
josernesto <jecm3099@gmail.com>
2019-04-06 12:27:08 -0400

    commit proyecto mvc

---
+++ ParcialFriend/ParcialFriend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ParcialFriend.MvcApplication" Language="C#" %>
---
+++ ParcialFriend/ParcialFriend/Models/JoseCalvimontesFriend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParcialFriend.Models
+{
+    public enum FriendType
+    {
+        Conocido,//0
+        CompañeroEstudio,//1
+        ColegaDeTrabajo,//2
+        Amigo,//3
+        AmigoDeInfancia,//4
+        Pariente//5
+
+
+
+    }
+
+
+    public class JoseCalvimontesFriend
+    {
+       [Required]
+        public int Name { get; set; }
+    
+    }
+}
